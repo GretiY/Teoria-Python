4749da18d4a9cab9e508ad5d04b9f65498c1f2bf
GretiY <gretiy2@gmail.com>
2026-03-05 13:34:55 +0100

    ejercicios

--- ejercicios.py
+++ ejercicios.py
@@ -252,4 +252,41 @@ else:
         print("hasta luego")
 
 
+#Ejercicio 18: validador de contraseñas 2.0
+
+print("Introduzca una contraseña segura que : ")
+print("Minimo 8 chars\nAl menos una mayuscula\nAl menos un numero\Al menos un simbolo ( ! , @ , #, $, %)")
+
+contraseña = input()
+longitud_ok = len(contraseña) >=8
+tiene_mayuscula = False
+tiene_numero = False
+tiene_simbolo = False
+simbolos = ["!", "@","$","%"]
+
+for caracter in contraseña:
+    if caracter.isupper():
+        tiene_mayuscula = True
+       
+    if caracter.isdigit():
+        tiene_numero = True
+        
+    
+    if caracter in simbolos:
+        tiene_simbolo = True
+        
+
+print(f"Longitud ok : {longitud_ok}")
+print(f"Tiene mayuscula : {tiene_mayuscula}")
+print(f"Tiene numero : {tiene_numero}")
+print(f"Tiene simbolo : {tiene_simbolo}")
+
+
+es_valida = longitud_ok and tiene_mayuscula and tiene_numero and tiene_simbolo
+
+if es_valida:
+    print(f"la contraseña {contraseña} es valida")
+else:
+    print(f"contraseña no valida")
+
 
--- pruebas .py
+++ pruebas .py
@@ -1,15 +1,35 @@
-autorizados = {"Carla", "Pedro", "Marta"}
-print("¿Cual es tu nombre?")
-nombre = input()
-if nombre in autorizados:
-    print(f"Bienvenido, {nombre}, estas en la lista de autorizados")
+#Ejercicio 18: 
+print("Introduzca una contraseña segura que : ")
+print("Minimo 8 chars\nAl menos una mayuscula\nAl menos un numero\Al menos un simbolo ( ! , @ , #, $, %)")
+
+contraseña = input()
+longitud_ok = len(contraseña) >=8
+tiene_mayuscula = False
+tiene_numero = False
+tiene_simbolo = False
+simbolos = ["!", "@","$","%"]
+
+for caracter in contraseña:
+    if caracter.isupper():
+        tiene_mayuscula = True
+       
+    if caracter.isdigit():
+        tiene_numero = True
+        
+    
+    if caracter in simbolos:
+        tiene_simbolo = True
+        
+
+print(f"Longitud ok : {longitud_ok}")
+print(f"Tiene mayuscula : {tiene_mayuscula}")
+print(f"Tiene numero : {tiene_numero}")
+print(f"Tiene simbolo : {tiene_simbolo}")
+
+
+es_valida = longitud_ok and tiene_mayuscula and tiene_numero and tiene_simbolo
+
+if es_valida:
+    print(f"la contraseña {contraseña} es valida")
 else:
-    print("¿Quieres añadir tu nombre a la lista?")
-    respuesta = input().lower()
-    if respuesta == "si":
-        autorizados.add(nombre)
-        print(autorizados)
-    else:
-        print("hasta luego")
-
-
+    print(f"contraseña no valida")
